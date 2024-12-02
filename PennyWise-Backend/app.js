9e9bb58bb2a80a2f2340de67412be72e0e1cf3ff
Akshat Sipany <borncancer21@gmail.com>
2024-12-02 20:55:08 +0530

    pushing Backend

---
+++ PennyWise-Backend/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { authRouter } = require('./routes/auth')
+const { expenseRouter } = require('./routes/expense')
+const { insightsRouter } = require('./routes/insights');
+app.use(express.json());
+app.use(cors());
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/expenses", expenseRouter);
+app.use("/api/v1/insights", insightsRouter);
+app.listen(3000, () => {
+  console.log("server is up on port 3000");
+});
